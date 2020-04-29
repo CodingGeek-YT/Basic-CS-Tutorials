14913dcfadc441a0fc928a5a93154b6196651ae5
CodingGeek-YT <64171495+CodingGeek-YT@users.noreply.github.com>
2020-04-29 10:40:31 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,5 @@ class Tutorial
     static void Main()
     {
         Console.WriteLine("Please input something.");
-        string input = Console.ReadLine();
-        Console.Write("Your input was "+input);
     }
 }

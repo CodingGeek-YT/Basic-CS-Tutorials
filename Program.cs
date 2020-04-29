cb44fb86da8a1d13dab3e3efd205f237b422c589
reNemesic <64527076+reNemesic@users.noreply.github.com>
2020-04-29 14:44:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Person
+{
+    public string Name;
+    public Person(string name)
+    {
+        Name = name;
+    }
+    static void Main()
+    {
+        Person p = new Person("Andy");
+        Console.Write(p.Name);
+    }
+}

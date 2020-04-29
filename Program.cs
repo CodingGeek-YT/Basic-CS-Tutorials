2dbade1eb16274a9359d18c56a812358e3a57efa
reNemesic <64527076+reNemesic@users.noreply.github.com>
2020-04-29 11:39:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Tutorial 
+{
+    static void Main()
+    {
+        Console.WriteLine("Please input something.");
+        string input = Console.ReadLine();
+        Console.Write("Your input was "+input);
+    }
+}
